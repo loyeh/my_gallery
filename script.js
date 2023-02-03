ad53507b49a72f7ee366a75b3defb06ed33d677b
Ahmad <ahmad@ahmd-asdy.local>
2023-02-03 04:52:09 +0330

    addin some effects when image pops up

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const container = document.createElement("div");
+const div = document.createElement("div");
 container.setAttribute("class", "grid_container");
 const popup = document.createElement("div");
-popup.setAttribute("class", "popup");
+popup.setAttribute("id", "popup");
+div.setAttribute("id", "overlay");
 const image = document.createElement("img");
 popup.appendChild(image);
 document.body.appendChild(container);
 document.body.appendChild(popup);
+document.body.appendChild(div);
 for (let i = 1; i < 52; i++) {
   const element = document.createElement("div");
   const image1 = document.createElement("img");
@@ -20,4 +23,5 @@ const images = document.getElementsByClassName("grid_element");
 function showImage(n) {
   image.setAttribute("src", `Img/piture_${n}.jpg`);
   popup.classList.toggle("active");
+  container.classList.toggle("blur");
 }
