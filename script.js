6131e660c3d31926151c8630e917aca58fe0c1b6
Ahmad <ahmad@ahmd-asdy.local>
2023-02-03 05:07:08 +0330

    addin some effects when image pops up

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const container = document.createElement("div");
-const div = document.createElement("div");
+const overlay = document.createElement("div");
 container.setAttribute("class", "grid_container");
 const popup = document.createElement("div");
 popup.setAttribute("id", "popup");
-div.setAttribute("id", "overlay");
+overlay.setAttribute("id", "overlay");
 const image = document.createElement("img");
 popup.appendChild(image);
 document.body.appendChild(container);
 document.body.appendChild(popup);
-document.body.appendChild(div);
+document.body.appendChild(overlay);
 for (let i = 1; i < 52; i++) {
   const element = document.createElement("div");
   const image1 = document.createElement("img");
@@ -22,6 +22,13 @@ for (let i = 1; i < 52; i++) {
 const images = document.getElementsByClassName("grid_element");
 function showImage(n) {
   image.setAttribute("src", `Img/piture_${n}.jpg`);
-  popup.classList.toggle("active");
-  container.classList.toggle("blur");
+  popup.classList.add("active");
+  overlay.classList.add("active");
+  container.classList.add("blur");
+}
+overlay.addEventListener("click", closeImage);
+function closeImage() {
+  popup.classList.remove("active");
+  overlay.classList.remove("active");
+  container.classList.remove("blur");
 }
