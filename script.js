99739c28601bfd8e88db1a2f506b4e135431399d
Ahmad <ahmad@ahmd-asdy.local>
2023-02-10 05:25:01 +0330

    mouse action adden,keyboard enter,esc keys defined

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const next = document.getElementById("next");
 const main = document.getElementById("main");
 const prev = document.getElementById("prev");
 const close = document.getElementById("close_button");
-let enlaged_image_number;
+let enlarged_image_number;
 const grid_elements = document.getElementsByClassName("grid_element");
 
 // generating the tumbnail grid in the document object
@@ -18,7 +18,7 @@ for (let i = 1; i < 52; i++) {
   image1.setAttribute("src", `Img/tumb/piture_${i}.jpg`);
   element.setAttribute("class", "grid_element");
   element.setAttribute("ondblclick", `showImage(${i})`);
-  image1.setAttribute("id", `item${i}`);
+  element.setAttribute("id", `item${i}`);
   element.append(image1);
   container.append(element);
 }
@@ -26,24 +26,23 @@ for (let i = 1; i < 52; i++) {
 function showImage(n) {
   image.setAttribute("src", `Img/piture_${n}.jpg`);
   background_image.setAttribute("src", `Img/tumb/piture_${n}.jpg`);
-
   popup.classList.add("active");
   overlay.classList.add("active");
   container.classList.add("blur");
-  enlaged_image_number = n;
-  if (enlaged_image_number > 50) {
+  enlarged_image_number = n;
+  if (enlarged_image_number > 50) {
     next.classList.add("hidden");
   }
-  if (enlaged_image_number >= 2) {
+  if (enlarged_image_number >= 2) {
     prev.classList.remove("hidden");
   }
-  if (enlaged_image_number < 2) {
+  if (enlarged_image_number < 2) {
     prev.classList.add("hidden");
   }
-  if (enlaged_image_number <= 50) {
+  if (enlarged_image_number <= 50) {
     next.classList.remove("hidden");
   }
-  return enlaged_image_number;
+  return enlarged_image_number;
 }
 
 function closeImage() {
@@ -54,20 +53,21 @@ function closeImage() {
 }
 
 function nextImage() {
-  enlaged_image_number++;
-  if (enlaged_image_number == 52) {
-    enlaged_image_number = 51;
+  enlarged_image_number++;
+  if (enlarged_image_number == 52) {
+    enlarged_image_number = 51;
   }
-  showImage(enlaged_image_number);
+  showImage(enlarged_image_number);
 }
 
 function previousImage() {
-  enlaged_image_number--;
-  if (enlaged_image_number == 0) {
-    enlaged_image_number = 1;
+  enlarged_image_number--;
+  if (enlarged_image_number == 0) {
+    enlarged_image_number = 1;
   }
-  showImage(enlaged_image_number);
+  showImage(enlarged_image_number);
 }
+
 function selectedImage(event) {
   for (let i = 0; i < grid_elements.length; i++) {
     grid_elements[i].classList.remove("selected");
@@ -84,6 +84,11 @@ function keyboardAction(event) {
   console.log(event);
   console.log(event.keyCode);
 
+  if (pressedKey == "Enter") {
+    let selectedImegeNumber = Number(selected[0].id.slice(-1));
+    showImage(selectedImegeNumber);
+  }
+
   if (pressedKey == "ArrowRight") {
     selected[0].nextElementSibling.classList.add("selected");
     selected[0].classList.remove("selected");
@@ -91,6 +96,31 @@ function keyboardAction(event) {
   if (pressedKey == "ArrowLeft") {
     selected[0].previousSibling.classList.add("selected");
     selected[1].classList.remove("selected");
+  }
+  let handler = selected[0].ondblclick;
+  console.log(handler);
+}
+
+function mouseAction(event) {
+  const selected = document.getElementsByClassName("selected");
+  let direction = event.deltaY;
+  if (direction > 0) {
+    selected[0].nextElementSibling.classList.add("selected");
+    selected[0].classList.remove("selected");
+  } else {
+    selected[0].previousSibling.classList.add("selected");
+    selected[1].classList.remove("selected");
+  }
+}
+
+function mouseScroll(event) {
+  console.log(event);
+  let direction = event.deltaY;
+  if ($("#popup").hasClass("active")) {
+    if (direction < 0) previousImage();
+    else nextImage();
+  } else {
+    mouseAction(event);
   }
 }
 
@@ -105,10 +135,43 @@ function keyboardScroll(event) {
     if (pressedKey == "ArrowLeft") {
       previousImage();
     }
+    if (pressedKey == "Escape") {
+      closeImage();
+    }
   } else {
     keyboardAction(event);
   }
 }
+
+function getGridData() {
+  // calc computed style
+  const gridComputedStyle = window.getComputedStyle(grid_container);
+
+  return {
+    // get number of grid rows
+    gridRowCount: gridComputedStyle.getPropertyValue("grid-template-rows").split(" ").length,
+    // get number of grid columns
+    gridColumnCount: gridComputedStyle.getPropertyValue("grid-template-columns").split(" ").length,
+    // get grid row sizes
+    gridRowSizes: gridComputedStyle.getPropertyValue("grid-template-rows").split(" ").map(parseFloat),
+    // get grid column sizes
+    gridColumnSizes: gridComputedStyle.getPropertyValue("grid-template-columns").split(" ").map(parseFloat),
+  };
+}
+
+window.addEventListener("DOMContentLoaded", outputGridData);
+window.addEventListener("resize", outputGridData);
+
+function outputGridData() {
+  const gridData = getGridData();
+  return {
+    Rows: gridData.gridRowCount,
+    Columns: gridData.gridColumnCount,
+    Rows_sizes: gridData.gridRowSizes,
+    Column_sizes: gridData.gridColumnSizes,
+  };
+}
+
 container.addEventListener("click", selectedImage);
 close.addEventListener("click", closeImage);
 next.addEventListener("click", nextImage);
